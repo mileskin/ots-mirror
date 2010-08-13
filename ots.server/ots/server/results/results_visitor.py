f0f65a4a0a356234cdbd4281fd972a2c45dc7c0b
tgalvin <ext-tom.galvin@nokia.com>
2010-08-13 17:37:14 +0100

    More epydocing and headering

--- ots.server/ots/server/results/results_visitor.py
+++ ots.server/ots/server/results/results_visitor.py
@@ -1,3 +1,34 @@
+# ***** BEGIN LICENCE BLOCK *****
+# This file is part of OTS
+#
+# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+#
+# Contact: Mikko Makinen <mikko.al.makinen@nokia.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+# ***** END LICENCE BLOCK *****
+
+
+"""
+Visitor for the Test Results XML as defined by
+
+http://gitorious.org/qa-tools/test-definition
+
+Pattern used to maintain backward compatibility
+"""
+
 import os
 
 import xml.etree.cElementTree as ET
@@ -5,6 +36,20 @@ import xml.etree.cElementTree as ET
 from ots.server.results.package_results_processor import PackageResultsProcessor
         
 def visit_results(results_xml, test_package, environment):
+    """
+    @type results_xml: C{string} 
+    @param results_xml: The results xml
+
+    @type results_xml: C{string} 
+    @param results_xml: The name of the test package 
+
+    @type results_xml: C{string} 
+    @param results_xml: The enviroment
+
+    @rtype: L{ots.common.api.PackageResults}
+    @return: A populated PackageResults
+    """
+
     visitor = ResultsVisitor()
     results_judge_processor = PackageResultsProcessor(test_package, environment)
     visitor.add_processor(results_judge_processor)
@@ -14,18 +59,32 @@ def visit_results(results_xml, test_package, environment):
 
 class ResultsVisitor(object):
     """
-    Extrinsic Visitor 
+    Extrinsic Visitor for the Test Results XML
     """
 
     _processors = []
 
     def add_processor(self, processor):
+        """
+        @type element: L{ots.server.results.ResultsProcessorBase} 
+        @param element: A results processor
+
+        Add a processor to accept nodes in tree traversal
+        """
+       
         self._processors.append(processor)
 
-    def visit(self, node):
+    def visit(self, element):
+        """
+        @type element: C{Element} 
+        @param element: An ElementTree Elemment 
+
+        Preorder Tree Traversal doing the 
+        'Pre' and 'Post' processing for the processors 
+        """
         for processor in self._processors:
-            processor.pre_process(node)
-        for child_node in node.getchildren():
+            processor.pre_process(element)
+        for child_node in element.getchildren():
             self.visit(child_node)
         for processor in self._processors:
-            processor.post_process(node)
+            processor.post_process(element)
--- ots.server/ots/server/results/tests/test_results_visitor.py
+++ ots.server/ots/server/results/tests/test_results_visitor.py
@@ -1,18 +1,49 @@
+# ***** BEGIN LICENCE BLOCK *****
+# This file is part of OTS
+#
+# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+#
+# Contact: Mikko Makinen <mikko.al.makinen@nokia.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+# ***** END LICENCE BLOCK *****
+
+
 import unittest
 
 import os
 
 from ots.server.results.results_visitor import visit_results, ResultsVisitor
 
-class TestResultsVisitor(unittest.TestCase):
+class TestVisitResults(unittest.TestCase):
 
-    def test_results_visitor(self):
+    def test_visit_results(self):
         dirname = os.path.dirname(os.path.abspath(__file__))
         results_file = os.path.join(dirname, "data", "dummy_results_file.xml")
         results_xml = open(results_file, "r").read()
         package_results =  visit_results(results_xml, "foo", "bar")
         print "sig", package_results.significant_results
         print "insig", package_results.insignificant_results
+
+class TestResultsVisitor(self):
+
+    def test_add_processor(self):
+        pass
+
+    def test_visit(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
