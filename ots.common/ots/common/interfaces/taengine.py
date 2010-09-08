47f7ee11dc6f1bdbd919a5698ad53c3b5891de38
Teemu Vainio <ext-teemu.a.vainio@nokia.com>
2010-09-08 10:41:41 +0300

    Moved post process plugin an TA Engine abstract base classes to ots tree

---
+++ ots.common/ots/common/interfaces/taengine.py
@@ -0,0 +1,53 @@
+# ***** BEGIN LICENCE BLOCK *****
+# This file is part of OTS
+#
+# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+#
+# Contact: Mikko Makinen <mikko.al.makinen@nokia.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+# ***** END LICENCE BLOCK ***** 
+
+
+"""
+OTS TA engine abstract base class.
+
+TA Engine is the interface for different test execution implementations.
+"""
+
+class TAEngine(object):
+    """OTS TA engine abstract base class"""
+
+        
+    def name(self):
+        """ This method returns the name of the plugin as a string
+                    
+            @rtype: C{string}
+            @return: Name of the plugin
+        """ 
+        raise NotImplementedError
+
+                
+    def execute(self, testrun_object):
+        """ This method launches the execution for a testrun
+    
+            @type testrun_object: L{Testrun}
+            @param testrun_object: The testrun to be executed
+                    
+            @rtype: C{None}
+            @return: Nothing
+
+        """ 
+        raise NotImplementedError
