42c24bf35208df2e43491981ba6d7b75980a23e0
tgalvin <ext-tom.galvin@nokia.com>
2010-09-13 14:23:55 +0100

    First commit for PluginBase.

---
+++ ots.common/ots/common/framework/plugin_base.py
@@ -0,0 +1,54 @@
+# ***** BEGIN LICENCE BLOCK *****
+# This file is part of OTS
+#
+# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+#
+# Contact: Mikko Makinen <mikko.al.makinen@nokia.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+# ***** END LICENCE BLOCK *****
+
+import os
+
+class PluginException(Exception):
+    pass
+
+class PluginBase(object):
+
+    DEFAULT_CONFIG_FILEPATH = "/etc/"
+
+    def __init__(self, application_id):
+        self.application_id = application_id
+
+    def _config_filename(self):
+        """
+        @rtype: C{str}
+        @rparam the config file path.
+
+        Tries /etc/APPLICATION_NAME.ini first.
+        If that does not work,
+        tries config.ini from ots.common directory
+        """
+        name = "%s.ini" % (self.application_id)
+        default = os.path.join(self.DEFAULT_CONFIG_FILEPATH, name)
+        if os.path.exists(default):
+            config_filename = default
+        else:
+            common_dirname = os.path.split(os.path.dirname(
+                               os.path.abspath(__file__)))[0]
+            config_filename = os.path.join(common_dirname, name)
+            if not os.path.exists(config_filename):
+                raise PluginException("%s not found"%(config_filename))
+        return config_filename
---
+++ ots.common/ots/common/framework/tests/test_plugin_base.py
@@ -0,0 +1,42 @@
+# ***** BEGIN LICENCE BLOCK *****
+# This file is part of OTS
+#
+# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+#
+# Contact: Mikko Makinen <mikko.al.makinen@nokia.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+# ***** END LICENCE BLOCK *****
+
+import os
+
+import unittest
+
+from ots.common.framework.plugin_base import PluginBase
+
+class TestPluginBase(unittest.TestCase):
+
+    def test_config_filename(self):
+        pi = PluginBase("config")
+        self.assertEquals(['ots', 'ots.common', 'ots', 'common', 'config.ini'],
+                          pi._config_filename().split("/")[-5:])
+        pi.DEFAULT_CONFIG_FILEPATH = os.path.dirname(os.path.abspath(__file__))
+        self.assertEquals(['ots', 'ots.common', 'ots', 'common',
+                           'framework', 'tests', 'config.ini'],
+                          pi._config_filename().split("/")[-7:])
+
+
+if __name__ == "__main__":
+    unittest.main()
