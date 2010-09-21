893ef897f672e1d1fb8fffae02aaffeb24fb51bf
tgalvin <ext-tom.galvin@nokia.com>
2010-09-21 18:10:13 +0100

    First commit with pickled classes

---
+++ ots.common/ots/common/message_io.py
@@ -0,0 +1,132 @@
+# ***** BEGIN LICENCE BLOCK *****
+# This file is part of OTS
+#
+# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+#
+# Contact: Mikko Makinen <mikko.al.makinen@nokia.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+# ***** END LICENCE BLOCK *****
+
+#WIP needs further simplification just cleaning the slate 
+
+"""
+The Message Types for sending down the wire and 
+the methods for packing and unpacking them
+"""
+
+from pickle import dumps, loads
+
+from amqplib import client_0_8 as amqp
+
+import ots.common
+from ots.common.resultobject import ResultObject
+
+DELIVERY_MODE = 2
+
+#########################
+# PACK / UNPACK
+#########################
+
+def unpack_message(message):
+    """
+    Unpack the message with Pickle
+
+    @type message: amqplib.client_0_8.basic_message.Message
+    @param message: A pickled message in AMQP message format
+
+    @rtype message: L{ots.common.message_io.Message} 
+    @rparam mesage: The Message 
+    """
+    body = loads(message.body)
+    return body
+
+def pack_message(message):
+    """
+    Packs the message for sending as AMQP with Pickle
+
+    @type message: L{ots.common.message_io.Message} 
+    @param mesage: The AMQP message 
+
+    @rtype message: amqplib.client_0_8.basic_message.Message
+    @return message: A pickled message in AMQP message format
+    """
+    message = dumps(message, True)
+    amqp_message = amqp.Message(message)
+    amqp_message.properties['delivery_mode'] = DELIVERY_MODE
+    return amqp_message
+
+##############################
+# MESSAGE TYPES 
+##############################
+
+class Message(object):
+    
+    __version__ = ots.common.__VERSION__
+
+class CommandMessage(Message):
+
+    def __init__(self, command, response_queue, task_id, 
+                 timeout = 60, min_worker_version = None):
+        """
+        @type command: C{list}
+        @param command: The CL params
+
+        @type queue : C{str}
+        @param queue : The queue to post the message
+
+        @type timeout : C{int}
+        @param timeout : The timeout for the Task
+
+        @type task_id : C{int}
+        @param task_id : The Task ID
+
+        @type min_worker_version: C{str}
+        @type min_worker_version: The minimum acceptable worker version 
+        """
+        self.command = " ".join(command)
+        self.response_queue = response_queue
+        self.task_id = task_id 
+        self.timeout = timeout 
+        self.min_worker_version = min_worker_version
+
+class ResultMessage(Message):
+
+    def __init__(self, filename, content, test_package, 
+                       origin, environment): 
+
+        self.result = ResultObject(filename,
+                              content,
+                              test_package,
+                              origin,
+                              environment)
+
+class StatusMessage(Message):
+
+    def __init__(self, state, status_info):
+        self.state = state
+        self.status_info = status_info
+
+class ErrorMessage(Message):
+    
+    def __init__(self, error_info, error_code):
+        self.error_info = error_info 
+        self.error_code = error_code 
+
+class TestPackageListMessage(Message):
+    
+    def __init__(self, environment, packages):
+        self.environment = environment
+        self.packages = packages
---
+++ ots.common/ots/common/tests/test_message_io.py
@@ -0,0 +1,75 @@
+# ***** BEGIN LICENCE BLOCK *****
+# This file is part of OTS
+#
+# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+#
+# Contact: Mikko Makinen <mikko.al.makinen@nokia.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+# ***** END LICENCE BLOCK *****
+
+import unittest 
+
+import ots.common
+
+from ots.common.resultobject import ResultObject 
+from ots.common.message_io import CommandMessage, ResultMessage
+from ots.common.message_io import StatusMessage, ErrorMessage
+from ots.common.message_io import TestPackageListMessage
+from ots.common.message_io import pack_message, unpack_message
+
+
+class TestMessageIO(unittest.TestCase):
+    
+    def test_command_message(self): 
+        cmd_msg = CommandMessage(["echo", "hello world"], 
+                                  "response_queue", "task_id") 
+        packed_msg = pack_message(cmd_msg)
+        rec_msg = unpack_message(packed_msg)
+        self.assertEquals("echo hello world", rec_msg.command)
+        self.assertEquals("response_queue", rec_msg.response_queue)
+        self.assertEquals("task_id", rec_msg.task_id)
+        self.assertEquals(ots.common.__VERSION__, rec_msg.__version__)
+
+    def test_result_message(self):
+        res_msg =  ResultMessage("foo.txt", "foo", "pkg_1", 
+                                 "unittest", "meego")
+        packed_msg = pack_message(res_msg)
+        rec_msg = unpack_message(packed_msg)
+        self.assertTrue(isinstance(rec_msg.result, ResultObject))
+
+    def test_status_message(self):
+        status_msg = StatusMessage("state", "status_info")
+        packed_msg = pack_message(status_msg)
+        rec_msg = unpack_message(packed_msg)
+        self.assertEquals("state", rec_msg.state)       
+        self.assertEquals("status_info", rec_msg.status_info)
+               
+    def test_error_message(self):
+        error_msg = ErrorMessage("error_info", "error_code")
+        packed_msg = pack_message(error_msg)
+        rec_msg = unpack_message(packed_msg)
+        self.assertEquals("error_info", rec_msg.error_info)       
+        self.assertEquals("error_code", rec_msg.error_code)
+
+    def test_test_package_list(self):
+        pkg_msg = TestPackageListMessage("environment", ["pkg1", "pkg2"])
+        packed_msg = pack_message(pkg_msg)
+        rec_msg = unpack_message(packed_msg)
+        self.assertEquals("environment", rec_msg.environment)       
+        self.assertEquals(["pkg1", "pkg2"], rec_msg.packages)
+
+if __name__ == "__main__":
+    unittest.main()
