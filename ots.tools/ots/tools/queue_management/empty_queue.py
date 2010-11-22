0b7498bcec5a5f92057d95482ae22f1fe8925957
tgalvin <tom@sarasola.co.uk>
2010-11-22 17:49:29 +0000

    Merge ots.tools

---
+++ ots.tools/ots/tools/queue_management/empty_queue.py
@@ -0,0 +1,56 @@
+# ***** BEGIN LICENCE BLOCK *****
+# This file is part of OTS
+#
+# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+#
+# Contact: Mikko Makinen <mikko.al.makinen@nokia.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+# ***** END LICENCE BLOCK *****
+
+"""
+Empty the queue of messages 
+"""
+import sys  
+
+from amqplib import client_0_8 as amqp
+
+def empty_queue(host, queue_name):
+    """Empty a queue in AMQP server"""
+    port = 5672
+    userid = "guest"
+    password = "guest"
+    virtual_host = "/"
+    connection = amqp.Connection(host = ("%s:%s" %(host, port)),
+                                 userid = userid,
+                                 password = password,
+                                 virtual_host = virtual_host,
+                                 insist = False)
+    channel = connection.channel()
+    channel.queue_purge(queue = queue_name, nowait=True)
+    #print dir(channel)
+def main():
+    """Main function"""
+    if len(sys.argv) != 3:
+        print "Usage python empty_queue host queue_name"
+        sys.exit()
+    host = sys.argv[1]
+    queue_name = sys.argv[2]
+    print host, queue_name
+    empty_queue(host, queue_name)
+
+
+if __name__ == "__main__":
+    main()
