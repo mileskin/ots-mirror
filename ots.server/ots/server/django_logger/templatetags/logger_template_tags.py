0bbd16818dad5bc97d409ae4a217d888d9f8e319
Esa-Pekka Miettinen <esa-pekka.miettinen@digia.com>
2010-11-23 10:56:02 +0200

    Added django http_logger

---
+++ ots.server/ots/server/django_logger/templatetags/logger_template_tags.py
@@ -0,0 +1,45 @@
+# ***** BEGIN LICENCE BLOCK *****
+# This file is part of OTS
+#
+# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+#
+# Contact: Mikko Makinen <mikko.al.makinen@nokia.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+# ***** END LICENCE BLOCK *****
+
+# Ignoring: Invalid name "register" 
+#           (should match (([A-Z_][A-Z1-9_]*)|(__.*__))$)
+# register should be written lowercase letters
+# pylint: disable-msg=C0103
+""" Template tags for logger
+"""
+import time, logging
+from django import template
+register = template.Library()
+
+@register.filter
+def convert_epoch_to_string(value):
+    """ Converts epoch value to string
+    """
+    return time.ctime(float(value))
+
+@register.filter
+def result_judge(levelname, levelnumber):
+    if int(levelnumber) >= logging.ERROR:
+        strOut = '<div class="red"><b>%s</b></div>' % levelname
+    else:
+        strOut = levelname
+    return strOut
---
+++ ots.server/ots/server/django_ots/extension_points.py
@@ -0,0 +1,79 @@
+# ***** BEGIN LICENCE BLOCK *****
+# This file is part of OTS
+#
+# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+#
+# Contact: Mikko Makinen <mikko.al.makinen@nokia.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+# ***** END LICENCE BLOCK *****
+
+"""
+Extension point example for Django OTS server
+"""
+
+from ots.server.testrun_db import interface
+
+
+
+def get_inputplugin(testrun):
+    """Returns custom input plugin"""
+    return None
+
+def get_postprocessplugins(testrun):
+    """Returns custom PostProcessPlugins as a list"""
+    return []
+
+def get_resultfilehandlers(testrun):
+    """Returns custom ResultFileHandlers as a list"""
+    return []
+
+def get_resultbackends(testrun):
+    """Returns custom ResultBackends as a list"""
+    return []
+
+#
+# DB related functions
+#
+
+def init_new_testrun(swproduct, testplan_id=None, testplan_name="",
+                    gate=None, label=None):
+    """
+    Creates a new testrun into DB and returns an unique testrun ID.
+    
+    @param swproduct: Name of the sw product this testrun belongs to
+    @type swproduct: C{string}
+
+    @return: Testrun id
+    @rtype: C{int}
+    """
+    return interface.init_new_testrun(swproduct,
+                                      testplan_id,
+                                      testplan_name,
+                                      gate,
+                                      label)
+
+
+def update_testrun_in_db(testrun, testrun_id):
+    """
+    This is called when testrun data in db needs updating
+    """
+    return interface.update_testrun(testrun, testrun_id)
+
+def get_testrun_link(testrun_id):
+    """
+    This returns a link to testrun results
+    """
+    return interface.get_testrun_link(testrun_id)
