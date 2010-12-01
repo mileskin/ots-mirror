b0985f54a593c4285de2900f5b94dce19f9781ce
Teemu Vainio <teemu.vainio@ixonos.com>
2010-12-01 15:01:51 +0200

    Created testcase for testing multiple devicegroups

--- ots.system_tests/log_tests.py
+++ ots.system_tests/log_tests.py
@@ -67,6 +67,10 @@ class TestErrorConditions(unittest.TestCase):
         options = Options()
         options.device = "devicegroup:this_should_not_exist"
         options.timeout = 1
+        print "Triggering a testrun with non existing devicegroup '%s'"\
+            % options.device
+        print "Please make sure the system does not have that devicegroup."
+
         result = ots_trigger(options)
 
         # Check the return value
@@ -91,6 +95,10 @@ class TestErrorConditions(unittest.TestCase):
         options = Options()
         options.sw_product = "this_should_not_exist"
         options.timeout = 1
+        print "Triggering a testrun with non existing sw_product '%s'"\
+            % options.sw_product
+        print "Please make sure the system does not have that sw_product."
+
         result = ots_trigger(options)
 
         # Check the return value
@@ -104,6 +112,47 @@ class TestErrorConditions(unittest.TestCase):
 
         string = """Incoming request: program: this_should_not_exist, request: 0, notify_list: ['tvainio@localhost'], options: {'engine': ['default'], 'image': 'http://dontcareabouttheimage', 'distribution_model': 'default', 'timeout': 1}"""
         self.assertTrue(has_message(testrun_id, string))
+
+class TestDeviceProperties(unittest.TestCase):
+
+    def test_multiple_devicegroups(self):
+        options = Options()
+        options.device = "devicegroup:this_should_not_exist;devicegroup:this_should_not_exist_either"
+        options.timeout = 1
+        print "Calling ots xmlrpc with multiple devicegroups: '%s'"\
+            % options.device
+        print "Please make sure the system does not have these devicegroups."
+        print "Checking that a separate testrun gets created for all devicegroups."
+        
+        old_testrun = get_latest_testrun_id()
+        result = ots_trigger(options)
+
+        # Check the return value
+        self.assertEquals(result, "FAIL")
+
+        testrun_id1 = get_second_latest_testrun_id()        
+        testrun_id2 = get_latest_testrun_id()
+
+
+        # Make sure we are not reading logs from previous runs
+        self.assertTrue(old_testrun not in (testrun_id1, testrun_id2))
+
+        self.assertTrue(has_errors(testrun_id1))
+        self.assertTrue(has_errors(testrun_id2))
+        
+        # Make sure correct routing keys are used
+        string = """Using routing key this_should_not_exist"""
+        self.assertTrue(has_message(testrun_id1, string))
+
+        string = """Using routing key this_should_not_exist_either"""
+        self.assertTrue(has_message(testrun_id2, string))
+
+        string = """'device': {'devicegroup': 'this_should_not_exist'}"""
+
+        self.assertTrue(has_message(testrun_id1, string))
+
+        string = """'device': {'devicegroup': 'this_should_not_exist_either'}"""
+        self.assertTrue(has_message(testrun_id2, string))
 
 
 ##################################
@@ -119,6 +168,21 @@ def get_latest_testrun_id():
     row1 = table.findAll("tr")[1]
     td = row1.findAll("td")[0].string
     return td
+def get_second_latest_testrun_id():
+    """
+    Scrape the second latest testrun id from the global log
+    """
+    latest = get_latest_testrun_id()
+    file =  urllib2.urlopen("%s"% GLOBAL_LOG)
+    soup = BeautifulSoup(file.read())
+    table =  soup.findAll("table")[1]
+    rows = table.findAll("tr")
+    for row in rows:
+        if row.findAll("td"):
+            td = row.findAll("td")[0].string
+            if td != latest:
+                return td
+    return None
 
 def has_message(testrun_id, string):
     """
