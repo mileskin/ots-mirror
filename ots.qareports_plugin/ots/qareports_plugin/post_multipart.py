b6c5637c0998dcbe5d43abfab34e6c5dea45a651
Teemu Vainio <teemu.vainio@ixonos.com>
2010-12-23 14:57:18 +0200

    Rename

--- ots.qareports_plugin/ots/qareports_plugin/post_multipart.py
+++ ots.qareports_plugin/ots/qareports_plugin/post_multipart.py
@@ -1,14 +1,19 @@
-# Based on http://code.activestate.com/recipes/146306/
+# Based on http://code.activestate.com/recipes/146306/ By Wade Leftwich
+"""
+functions for multipart POST file sending
+"""
+
 import httplib, mimetypes
 
 def post_multipart(host, selector, fields, files):
     """
     Post fields and files to an http host as multipart/form-data.
     fields is a sequence of (name, value) elements for regular form fields.
-    files is a sequence of (name, filename, value) elements for data to be uploaded as files
+    files is a sequence of (name, filename, value) elements for data to be
+    uploaded as files
     Return the server's response page.
     """
-    content_type, body = encode_multipart_formdata(fields, files)
+    content_type, body = _encode_multipart_formdata(fields, files)
     h = httplib.HTTP(host)
     h.putrequest('POST', selector)
     h.putheader('content-type', content_type)
@@ -18,10 +23,11 @@ def post_multipart(host, selector, fields, files):
     errcode, errmsg, headers = h.getreply()
     return h.file.read()
 
-def encode_multipart_formdata(fields, files):
+def _encode_multipart_formdata(fields, files):
     """
     fields is a sequence of (name, value) elements for regular form fields.
-    files is a sequence of (name, filename, value) elements for data to be uploaded as files
+    files is a sequence of (name, filename, value) elements for data to be
+    uploaded as files
     Return (content_type, body) ready for httplib.HTTP instance
     """
     BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
@@ -34,8 +40,9 @@ def encode_multipart_formdata(fields, files):
         L.append(value)
     for (key, filename, value) in files:
         L.append('--' + BOUNDARY)
-        L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
-        L.append('Content-Type: %s' % get_content_type(filename))
+        L.append('Content-Disposition: form-data; name="%s"; filename="%s"'\
+                     % (key, filename))
+        L.append('Content-Type: %s' % _get_content_type(filename))
         L.append('')
         L.append(value)
     L.append('--' + BOUNDARY + '--')
@@ -44,7 +51,5 @@ def encode_multipart_formdata(fields, files):
     content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
     return content_type, body
 
-def get_content_type(filename):
+def _get_content_type(filename):
     return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
-
-
