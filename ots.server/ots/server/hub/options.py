0b7aa6c495b219f86edebc1b0dabc8267f202293
Teemu Vainio <teemu.vainio@ixonos.com>
2011-01-21 11:21:32 +0200

    Fixed distribution model verification

--- ots.server/ots/server/hub/options.py
+++ ots.server/ots/server/hub/options.py
@@ -82,7 +82,9 @@ class Options(object):
         self.input_plugin = input_plugin # Deprecated
         self._timeout = timeout
         self._validate_packages(self.hw_packages)
-        self._validate_distribution_models(distribution_model, packages)
+
+        all_packages = self.hw_packages + self.host_packages
+        self._validate_distribution_models(distribution_model, all_packages)
 
 
     ##################################
