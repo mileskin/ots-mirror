c86cb22d8ac6ace3f0e61e96230f30ecd1f1463e
Teemu Vainio <teemu.vainio@ixonos.com>
2011-01-24 14:32:30 +0200

    Changed email error messages to warnings. They are not concidered critical.

--- ots.email_plugin/ots/email_plugin/email_plugin.py
+++ ots.email_plugin/ots/email_plugin/email_plugin.py
@@ -257,13 +257,13 @@ class EmailPlugin(PublisherPluginBase):
             except smtplib.SMTPRecipientsRefused:
                 failed_addresses = self._notify_list
             except socket.gaierror:
-                LOG.error("Invalid or unknown SMTP host")
+                LOG.warning("Invalid or unknown SMTP host")
             finally:
                 if mail_server is not None:
                     mail_server.close()
             if failed_addresses:
                 
-                LOG.error("Error in sending mail to following addresses:")
-                LOG.error(str(failed_addresses)) 
+                LOG.warning("Error in sending mail to following addresses:")
+                LOG.warning(str(failed_addresses)) 
         else:
-            LOG.error("No address list")
+            LOG.warning("No address list")
