e98b0623366cab4ae8d6c2f60c830998531eb58d
Ville Niutanen <ville.niutanen@digia.com>
2011-06-07 07:24:04 +0300

    Added paging to logger main page

--- ots.plugin.logger/ots/plugin/logger/views.py
+++ ots.plugin.logger/ots/plugin/logger/views.py
@@ -52,10 +52,35 @@ from copy import deepcopy
 from django.http import HttpResponse
 from django.conf import settings
 from django.template import loader, Context
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 from ots.plugin.logger.models import LogMessage
 
 ROW_AMOUNT_IN_PAGE = 50
+
+def _paginate(request,list_items):
+    """paginates list of items
+
+        @type request: C{HttpResponse}
+        @param request: HttpRequest of the view
+
+        @type list_items: C{list} or C{QuerySet}
+        @param list_items: list of items to be paginated
+
+        @rtype: C{list} or C{QuerySet}
+        @return: Returns items on page
+    
+    """
+    paginator = Paginator(list_items,ROW_AMOUNT_IN_PAGE)
+    try: 
+        page = int(request.GET.get('page','1'))
+    except ValueError:
+        page = 1
+    try:
+        list = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        list = paginator.page(paginator.num_pages)
+    return list
 
 def create_message(request, servicename=None, run_id=None):
     """ Creates log message to database.
@@ -332,7 +357,8 @@ def main_page(request):
     }
 
     messages = LogMessage.objects.get_latest_messages().order_by('-created')
-    context_dict['messages']  = messages
+    
+    context_dict['messages']  = _paginate(request,messages)
     template = loader.get_template('logger/index.html')
     return HttpResponse(template.render(Context(context_dict)))
 
