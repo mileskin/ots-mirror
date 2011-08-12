6a77d5d2a0c575fd97a3da8ee3925aa74c904a34
Oskari Pirttikoski <ext-oskari.o.pirttikoski@nokia.com>
2011-08-12 11:25:48 +0300

    move common functionality to reusable script

---
+++ ci/jenkins/prepare-pythonpath.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Adds OTS code and django_xmlrpc in PYTHONPATH
+mkdir -p $WORKSPACE/eggs
+export PYTHONPATH=$WORKSPACE/eggs
+PACKAGES="ots.common ots.results ots.server ots.worker ots.tools ots.django ots.plugin.email ots.plugin.logger ots.plugin.qareports ots.plugin.monitor ots.plugin.history ots.plugin.conductor.richcore"
+for egg_root in $PACKAGES; do
+  (cd $WORKSPACE/$egg_root && python setup.py develop --install-dir=$WORKSPACE/eggs)
+done
+export DJANGO_SETTINGS_MODULE=ots.django.settings
+rm -rf $WORKSPACE/eggs/django_xmlrpc
+# Expect to find preloaded django_xmlrpc-0.1.tar.gz in users home directory
+tar xzf ~/django_xmlrpc-0.1.tar.gz -C $WORKSPACE/eggs
