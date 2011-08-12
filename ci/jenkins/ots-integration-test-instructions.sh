5590b5e26370bfdece718c1c7aab996b4247ee19
Oskari Pirttikoski <ext-oskari.o.pirttikoski@nokia.com>
2011-08-12 12:13:39 +0300

    execute scripts so that the env variables set in them are visible also to the caller

--- ci/jenkins/ots-integration-test-instructions.sh
+++ ci/jenkins/ots-integration-test-instructions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Set PYTHONPATH and run the tests
-$WORKSPACE/ci/jenkins/prepare-pythonpath.sh
+. $WORKSPACE/ci/jenkins/prepare-pythonpath.sh
 python ots.django/ots/django/manage.py test --settings=settings logger history
 
--- ci/jenkins/ots-unit-test-instructions.sh
+++ ci/jenkins/ots-unit-test-instructions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Remove logs, set PYTHONPATH and run the tests
 rm -f /var/log/ots/*
-$WORKSPACE/ci/jenkins/prepare-pythonpath.sh
+. $WORKSPACE/ci/jenkins/prepare-pythonpath.sh
 $WORKSPACE/nose.sh --with-xunit --xunit-file=$WORKSPACE/nosetests.xml
 
