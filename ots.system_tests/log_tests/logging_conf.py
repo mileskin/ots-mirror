c148e16976f968cbd8ae31062b66905a6cfbf7d4
Miki Leskinen <ext-miki.leskinen@nokia.com>
2011-08-18 16:24:35 +0300

    Fix date format, now 4PM is 16 instead of 04

--- ots.system_tests/log_tests/logging_conf.py
+++ ots.system_tests/log_tests/logging_conf.py
@@ -2,6 +2,6 @@ import logging as log
 
 log.basicConfig(
     format='[%(asctime)s] %(levelname)-7s - %(message)s',
-    datefmt='%Y-%m-%d %I:%M:%S',
+    datefmt='%Y-%m-%d %H:%M:%S',
     level=log.DEBUG)
 
