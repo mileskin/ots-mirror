ababccfff48155b4ca532ebe78e86fd227131191
Miki Leskinen <ext-miki.leskinen@nokia.com>
2011-08-23 15:30:02 +0300

    Add simple script for running all system tests in parallel

---
+++ ots.system_tests/log_tests/run-all-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Run all system tests in parallel using nose > 1.1.0.
+#
+# For best speed the number of processes should be equal to the number
+# of devices available. It should not be higher, that causes timeouts.
+#
+# For detailed nosetests configuration see
+# http://readthedocs.org/docs/nose/en/latest/plugins/multiprocess.html
+#
+# Run single test:  ... log_tests.py:TestCustomDistributionModels.test_load_invalid_distribution_model
+# Run single suite: ... log_tests.py:TestErrorConditions
+# Run all tests:    ... log_tests.py
+#
+
+nosetests-2.6 --nologcapture --processes=9 --process-timeout=3600 --process-restartworker log_tests.py &
+
